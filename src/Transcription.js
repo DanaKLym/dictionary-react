4ecd97b307308b67c274a9c5f159e9eedb6b56e4
Dana Klymenko <bohdanamandziuk@gmail.com>
2023-02-28 13:34:57 +0200

    updated transcription symbols

--- src/Transcription.js
+++ src/Transcription.js
@@ -4,7 +4,7 @@ export default function Transcription(props) {
 
     if(props.transcription[0].hwi.prs != null) {
         return (
-            <div className="transcription">[{props.transcription[0].hwi.prs[0].mw.replaceAll(`sh`, `ʃ`).replaceAll(`j`, `ʤ`).replaceAll(`ch`, `ʧ`).replaceAll(`zh`, `ʒ`).replaceAll(`th`, `Ɵ`).replaceAll(`t͟h`, `ð`).replaceAll(`ā`, `ei`).replaceAll('ī', 'ai').replaceAll('ȯ', 'ɔː')}]</div>
+            <div className="transcription">[{props.transcription[0].hwi.prs[0].mw.replaceAll(`sh`, `ʃ`).replaceAll(`j`, `ʤ`).replaceAll(`ch`, `ʧ`).replaceAll(`zh`, `ʒ`).replaceAll(`th`, `Ɵ`).replaceAll(`t͟h`, `ð`).replaceAll(`ā`, `ei`).replaceAll('ä', 'ɑː').replaceAll('ō', 'əʊ').replaceAll('ȯ', 'ɔː').replaceAll('ē', 'i:').replaceAll('ī', 'ai').replaceAll('u̇', 'ʊ').replaceAll('ü', 'u:')}]</div>
         );
     } else {
         return  null;
